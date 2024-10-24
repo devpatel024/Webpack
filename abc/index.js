5bb9aad37e4267751c21392fe35390451b61fdc9
Dev <devpatel41203@gmail.com>
2024-10-24 10:46:20 +0530

    setup done!

--- abc/index.js
+++ abc/index.js
@@ -1,4 +1,17 @@
-const sum = require('./sum');
+require("./style.css");
+require("./app.css");
+
+const sum = require("./sum");
+
+const str = "Hello World!";
+console.log(str);
+function test() {
+    return "hello world"
+}
+
+
+console.log(test());
+
 
 const a = 5;
 const b = 5;
